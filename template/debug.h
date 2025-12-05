922ac1e1030ce9a8771e3a461f33e81a7cd5eae9
Aldo1919xd <kaledsama500@gmail.com>
2025-12-05 16:11:35 -0500

    Initial commit

---
+++ template/debug.h
@@ -0,0 +1,107 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = ", print(x), cerr << '\n';
+#else
+#define dbg(x)
+#endif
+
+// ==================== PRIMITIVOS ====================
+inline void print(int x) { cerr << x; }
+inline void print(long x) { cerr << x; }
+inline void print(long long x) { cerr << x; }
+inline void print(unsigned x) { cerr << x; }
+inline void print(unsigned long long x) { cerr << x; }
+inline void print(float x) { cerr << x; }
+inline void print(double x) { cerr << x; }
+inline void print(long double x) { cerr << x; }
+inline void print(char x) { cerr << '\'' << x << '\''; }
+inline void print(const char *x) {
+  if (x)
+    cerr << '\"' << x << '\"';
+  else
+    cerr << "null";
+}
+inline void print(const string &x) { cerr << '\"' << x << '\"'; }
+inline void print(bool x) { cerr << (x ? "true" : "false"); }
+
+// ==================== PARES Y TUPLAS ====================
+template <typename T, typename U> inline void print(const pair<T, U> &p) {
+  cerr << '(';
+  print(p.first);
+  cerr << ", ";
+  print(p.second);
+  cerr << ')';
+}
+
+template <typename... Ts> inline void print(const tuple<Ts...> &t) {
+  cerr << '(';
+  apply(
+      [](auto const &...elems) {
+        size_t n = 0;
+        ((print(elems), cerr << (++n < sizeof...(Ts) ? ", " : "")), ...);
+      },
+      t);
+  cerr << ')';
+}
+
+template <size_t N> inline void print(const bitset<N> &b) {
+  cerr << '"' << b.to_string() << '"';
+}
+
+// ==================== TRAITS ====================
+// Iterable detection
+template <typename T, typename = void> struct is_iterable : false_type {};
+
+template <typename T>
+struct is_iterable<
+    T, void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))>>
+    : true_type {};
+
+template <typename T> constexpr bool is_iterable_v = is_iterable<T>::value;
+
+// String detection (para excluir string en contenedores)
+template <typename T>
+constexpr bool is_string_like_v = is_same<decay_t<T>, string>::value;
+
+// Map-like detection
+template <typename T, typename = void> struct is_map_like : false_type {};
+
+template <typename T>
+struct is_map_like<T, void_t<typename T::key_type, typename T::mapped_type>>
+    : true_type {};
+
+// ==================== CONTENEDORES ====================
+// Genérico (excepto string y map-like)
+template <typename T>
+inline typename enable_if<is_iterable_v<T> && !is_string_like_v<T> &&
+                          !is_map_like<T>::value>::type
+print(const T &container) {
+  cerr << '{';
+  for (auto it = container.begin(); it != container.end(); ++it) {
+    print(*it);
+    if (next(it) != container.end())
+      cerr << ", ";
+  }
+  cerr << '}';
+}
+
+// Map-like
+template <typename M>
+inline typename enable_if<is_map_like<M>::value>::type print(const M &m) {
+  cerr << '{';
+  for (auto it = m.begin(); it != m.end(); ++it) {
+    print(it->first);
+    cerr << ": ";
+    print(it->second);
+    if (next(it) != m.end())
+      cerr << ", ";
+  }
+  cerr << '}';
+}
+
+#endif // DEBUG_H
---
+++ template/main.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <class T> using OSet = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class Key, class Value> using OMap = tree<Key, Value, less<Key>, rb_tree_tag, tree_order_statistics_node_update>;
+struct custom_hash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; 
+x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31);}size_t operator()(uint64_t x) const { 
+static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM);}};
+template <typename K, typename V> using Hash = gp_hash_table<K, V, custom_hash>;
+ 
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvl = vector<vll>;
+ 
+const int INF = 1e9 ;
+const ll LINF = 1e18 ;
+const ld EPS = 1e-9;
+const int MXN = 200000 + 5;
+const int MOD = 1e9 + 7;
+const int MOD2 = 998244353;
+const int MOD3 = 1e9 + 9;
+const ld PI = acos(-1.0);
+const int dx[] = {1, 0, -1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy8[] = {0, 1, 0, -1, 1, -1, 1, -1};
+ 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define approx(a) fixed << setprecision(a)
+#define sz(x) (int)(x).size()
+#define fastIO()                                                               \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(nullptr);                                                            \
+  cout.tie(nullptr)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define getBit(S, i) ((S >> i) & 1)
+#define setBit(S, i) (S | (1LL << i))
+#define clearBit(S, i) (S & ~(1LL << i))
+#define toggleBit(S, i) (S ^ (1LL << i))
+#define bit(i) (1LL << i)
+#define assignBit(S,i,v) ( ((S) & ~(1LL << (i))) | ((1LL * (v)) << (i)) )
+#define isPowerOfTwo(S) (!(S & (S - 1)) && S)
+#define LSB(S) (S & (-S)) 
+#define msb(S) (63 - __builtin_clzll(S))
+#define mem(a, value) memset(a, value, sizeof(a))
+ 
+template <typename T> inline T ceildiv(T a, T b) { return (a + b - 1) / b; }
+
+void solve() {
+  
+
+}
+
+int main() {
+  fastIO();
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
+
